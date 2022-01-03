a6a721929e89d63834b24bf87fbe928db6e13700
Mohammed SHAFI Uddin <95120398+zaroon427@users.noreply.github.com>
2022-01-03 19:22:18 +0530

    Delete firebase.js

--- firebase.js
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import firebaseConfig from "./firebaseConfig";
-
-const firebaseApp = initializeApp(firebaseConfig);
-
-const db = getFirestore(firebaseApp);
-
-export { db, firebaseApp };
